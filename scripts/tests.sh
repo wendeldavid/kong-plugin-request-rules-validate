5cc7195e7ac91e60a2fb619e13f579300de7c218
wendeldavid <wendel.przygoda@rdstation.com>
2023-12-15 16:43:03 -0300

    run all tests script

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -6,11 +6,7 @@ export KONG_LOG_LEVEL=debug
 rm -f luacov.report.out
 rm -f luacov.stats.out
 
-
-# pongo run -v -- ./spec/request-rules-validator/01-unit_spec.lua --coverage
-pongo run -v -- ./spec/request-rules-validator/02-integration_spec.lua
-
 # all tests
-# pongo run -v -- --coverage
+pongo run -v -- --coverage
 
 pongo down
